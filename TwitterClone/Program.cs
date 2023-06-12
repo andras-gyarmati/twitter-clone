cf1df67fdbb2d9495f60538456bcffd058934142
Andras Gyarmati <bandieee16@gmail.com>
2023-06-12 19:53:09 +0200

    add swagger auth token modal

--- TwitterClone/Program.cs
+++ TwitterClone/Program.cs
@@ -2,9 +2,13 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace TwitterClone;
 
@@ -49,7 +53,20 @@ public class Program
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiPlayground", Version = "v1" });
+            c.AddSecurityDefinition("token", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                In = ParameterLocation.Header,
+                Name = HeaderNames.Authorization,
+                Scheme = "Bearer"
+            });
+            // dont add global security requirement
+            // c.AddSecurityRequirement(/*...*/);
+            c.OperationFilter<SecureEndpointAuthRequirementFilter>();
+        });
 
         // builder.Services.AddDbContext<TwitterCloneDbContext>(options => options.UseSqlite());
         builder.Services.AddDbContext<TwitterCloneDbContext>();
@@ -78,6 +95,31 @@ public class Program
         app.UseAuthorization();
         app.MapControllers();
         app.Run();
+    }
+
+    internal class SecureEndpointAuthRequirementFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (!context.ApiDescription
+                    .ActionDescriptor
+                    .EndpointMetadata
+                    .OfType<AuthorizeAttribute>()
+                    .Any())
+            {
+                return;
+            }
+            operation.Security = new List<OpenApiSecurityRequirement>
+            {
+                new OpenApiSecurityRequirement
+                {
+                    [new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "token" }
+                    }] = new List<string>()
+                }
+            };
+        }
     }
 
     private class SlugifyParameterTransformer : IOutboundParameterTransformer
